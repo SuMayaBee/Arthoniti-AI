4bd69ca6445e4bf52ba00ec782ccfb1655adc10a
arafatDU <bsse1435@iit.du.ac.bd>
2025-09-19 15:50:47 +0600

    Auth service implemented

---
+++ backend/auth-service/src/main/java/com/aibusiness/auth/entity/User.java
@@ -0,0 +1,57 @@
+package com.aibusiness.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user") // "user" is a reserved keyword in PostgreSQL
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String email;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(); // We are not using roles in this simple setup
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ backend/auth-service/src/main/java/com/aibusiness/auth/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.aibusiness.auth.repository;
+
+import com.aibusiness.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
